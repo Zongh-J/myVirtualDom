6028e142999dd707db981efb62fc8a5f8e7b6393
jiaozonghao <352457175@qq.com>
2021-03-14 16:31:41 +0800

    新旧节点不同text的情况

--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import h from './mySnabbdom/h.js'
 import dispatch from './mySnabbdom/diapatch.js' 
 
 
-//const mySnabbdom = h('h1', {}, '我是一个盒子')
+const mySnabbdom = h('h1', {}, '我是一个盒子')
 //const mySnabbom3 = h('div', {}, h('span', {}, 'xx'))
 
-const mySnabbdom = h('h1', {}, [
-  h ('span', {}, '哈哈'),
-  h ('span', {}, '嘻嘻')
+// const mySnabbdom = h('ul', {}, [
+//   h ('li', {}, '哈哈'),
+//   h ('li', {}, '嘻嘻')
+// ])
+
+const mySnabbdom1 = h('h1', {}, [
+  h ('p', {}, '哈哈'),
+  h ('p', {}, '嘻嘻')
 ])
 
 
 //获取container元素
 const container = document.getElementById ('container')
+//获取btn元素
+const btnNode = document.getElementById ('btn')
 
 //调用dispatch函数
 dispatch (container, mySnabbdom)
+
+btnNode.onclick = function () { 
+  dispatch (mySnabbdom, mySnabbdom1)
+ }
--- src/mySnabbdom/diapatch.js
+++ src/mySnabbdom/diapatch.js
@@ -8,8 +8,33 @@ function dispatch (oldVNode, newVNode) {
     oldVNode = VNode (sel, {}, undefined, undefined, oldVNode)
   } 
   if (oldVNode.key == newVNode.key && oldVNode.sel == newVNode.sel) { //是同一个节点，精细化比较
-   
+    // 对比新旧节点是否为同一个对象
+    if (oldVNode === newVNode) return;
+    //新节点是否有text属性
+    if (newVNode.text && (newVNode.children == undefined || newVNode.children.length === 0)) {
+      //判断新旧节点的text属性是否相同
+      if (newVNode.text !== oldVNode.text) {
+        //新节点的text属性赋值给旧节点的innerText
+        oldVNode.elm.innerText = newVNode.text
+      }
+    } else {//新节点没有text，有children属性
+      //旧节点是否children属性
+      if (Array.isArray(oldVNode.children) && oldVNode.children.length > 0) {
+        //最难的一块
+        console.log('旧节点有children属性');
 
+
+
+      } else { //旧节点是text，将新节点的children赋值给旧节点
+        //1. 将旧节点的innerText变为空
+        oldVNode.elm.innerText = ''
+        //2. 遍历新节点的children，将新节点的children的属性插入到旧节点中
+        for (let i = 0; i < newVNode.children.length; i++) {
+          const dom = createElement (newVNode.children[i])
+          oldVNode.elm.appendChild (dom)
+        }
+      }
+    }
   } else { //不是同一个节点，暴力拆除旧的，装上新的
     const newVNodeDOM = createElement (newVNode)
     if (oldVNode.elm && newVNodeDOM) {
